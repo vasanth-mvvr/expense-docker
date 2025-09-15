3d6b68e20cb4219930f5190a2918ac8daff3c8e1
vasanth-mvvr <veeravasanthreddy2003@gmail.com>
2025-09-15 16:41:11 +0530

    latest

---
+++ docker-install.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+R="\e[31m"
+N="\e[0m"
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+systemctl start docker
+systemctl enable docker
+
+echo -e "$R logout and login again $n"
