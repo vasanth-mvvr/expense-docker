062e5e243ac4284dfe4dd38348ee859ddc0f0088
vasanth-mvvr <veeravasanthreddy2003@gmail.com>
2025-09-15 16:47:10 +0530

    latest

--- docker-install.sh
+++ docker-install.sh
@@ -4,9 +4,10 @@ R="\e[31m"
 N="\e[0m"
 sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 systemctl start docker
 systemctl enable docker
 
+usermod -aG docker ec2-user
 echo -e "$R logout and login again $n"
